dd075388f511147f98e8345fc655110207eaf65d
Brad Hambrice <hambrice@uab.edu>
2018-01-11 21:41:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,4 +15,20 @@ class Song
     song
   end
   
+  def self.new_from_filename(some_filename)
+  file_hash = {Artist => some_filename.split(" - ")[0], Genre => some_filename.split(" - ")[2].gsub!(".mp3", "")}
+ file_array = file_hash.collect do |key,value|
+  key.find_by_name(value) || key.new(value) 
+end
+Song.new(some_filename.split(" - ")[1], file_array[0], file_array[1])
+#binding.pry
+
+ # Artist.find_by_name(
+ #= Artist.new(some_filename.split(" - ")[0])
+ # genre = Genre.new(some_filename.split(" - ")[2].gsub!#(".mp3", ""))
+  # song = Song.new(some_filename.split(" - ")[1], artist#, genre)
+ 
+  
+  
+  end
 end
